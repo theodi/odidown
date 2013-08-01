4193d95bd0bc9565f7e83d3e6b5a6fb49a41039d
James Smith <james@floppy.org.uk>
2013-08-01 13:15:57 +0100

    Add soundcloud embeds

--- lib/odidown/extensions.rb
+++ lib/odidown/extensions.rb
@@ -1,7 +1,12 @@
 class Govspeak::Document
 
   extension('youtube', surrounded_by("youtube[","]")) do |body|
-    %{<iframe width="560" height="315" src="//www.youtube.com/embed/#{body}" frameborder="0" allowfullscreen=""></iframe>}
+    %{<div><iframe width="560" height="315" src="//www.youtube.com/embed/#{body}" frameborder="0" allowfullscreen=""></iframe></div>}
+  end
+
+  extension('soundcloud', surrounded_by("soundcloud[","]")) do |body|
+    %{<div><iframe width="100%" height="166" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F#{body}"></iframe></div>
+}
   end
 
 end
--- test/extensions_test.rb
+++ test/extensions_test.rb
@@ -7,5 +7,11 @@ class ExtensionsTest < Test::Unit::TestCase
     target_html = '<iframe width="560" height="315" src="//www.youtube.com/embed/OZPTM0PGQPE" frameborder="0" allowfullscreen=""></iframe>'
     assert Govspeak::Document.new(odidown).to_html.include? target_html
   end
-
+  
+  test "soundcloud extension" do
+    odidown = 'soundcloud[81761778]'
+    target_html = '<iframe width="100%" height="166" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F81761778"></iframe>'
+    assert Govspeak::Document.new(odidown).to_html.include? target_html
+  end
+  
 end
